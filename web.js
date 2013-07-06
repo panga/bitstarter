37ad59a5fd50240a51882d6a0dbfbe446d76f781
Leonardo Loch Zanivan <pangalz@gmail.com>
2013-07-06 08:33:55 -0400

    index

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
-var express = require('express');
+var express = require('express'),
+    fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+app.get('/', handler);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
